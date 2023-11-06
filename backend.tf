1cf8d1d48f74a0c61e33360e82f126fc6e39f0af
Arya Kumar Arya <144605735+Arya5596@users.noreply.github.com>
2023-11-07 02:03:11 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,6 @@
+backend "azurerm" {
+  resource_group_name   = "group_az"
+  storage_account_name  = "dummyterraform"
+  container_name        = "terrraformstate"
+  key                   = "sample/vm"
+}
